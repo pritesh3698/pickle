04846aaaad658d184fb5ef05f75e2c10397d1c4f
Pritesh Mahajan <105488969+pritesh3698@users.noreply.github.com>
2023-11-23 14:45:18 +0530

    Update Prometheus_matrix.py

--- Prometheus_matrix.py
+++ Prometheus_matrix.py
@@ -1,8 +1,13 @@
+# prometheus_exposer.py
+
 from prometheus_client import start_http_server, Gauge
 import time
 
 # Create a Prometheus Gauge metric
 predictions_metric = Gauge('release_date_predictions', 'Predicted release dates')
+
+# Replace this with the actual value obtained from predictor.py or an API call
+sample_predictions = 42
 
 # Expose predictions as metrics
 def expose_predictions(predictions):
@@ -13,5 +18,6 @@ start_http_server(8000)
 
 # Expose predictions periodically (adjust as needed)
 while True:
-    expose_predictions(predictions)
+    # Replace this with the actual value obtained from predictor.py or an API call
+    expose_predictions(sample_predictions)
     time.sleep(60)  # Update every 60 seconds
